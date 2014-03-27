772f29f9cbfe2592e9d5f9790c0cd32455bf2b7b
Nyakeh Rogers <nyakeh123@gmail.com>
2014-03-27 16:01:21 +0000

    Settings Page buttons now perform relevant actions.

--- Gauge/gen/com/example/gauge/R.java
+++ Gauge/gen/com/example/gauge/R.java
@@ -151,25 +151,25 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
-        public static final int action_websearch=0x7f050010;
+        public static final int action_websearch=0x7f050014;
         public static final int app_name=0x7f050000;
         public static final int calculate_header=0x7f050004;
         public static final int calculation_results_header=0x7f050006;
         public static final int compare_header=0x7f050005;
         public static final int comparison_results_header=0x7f050007;
-        public static final int drawer_close=0x7f05000e;
-        public static final int drawer_open=0x7f05000d;
+        public static final int drawer_close=0x7f050012;
+        public static final int drawer_open=0x7f050011;
         public static final int hello_world=0x7f050002;
-        public static final int menu=0x7f05000f;
-        public static final int title_activity_account=0x7f050013;
+        public static final int menu=0x7f050013;
+        public static final int title_activity_account=0x7f05000f;
         public static final int title_activity_calculate=0x7f050008;
         public static final int title_activity_calculation_result=0x7f050009;
         public static final int title_activity_compare=0x7f05000c;
-        public static final int title_activity_comparison_results=0x7f050012;
+        public static final int title_activity_comparison_results=0x7f05000e;
         public static final int title_activity_drawer=0x7f05000b;
-        public static final int title_activity_landing=0x7f050011;
+        public static final int title_activity_landing=0x7f05000d;
         public static final int title_activity_register=0x7f05000a;
-        public static final int title_activity_setting=0x7f050014;
+        public static final int title_activity_setting=0x7f050010;
         public static final int welcome_header=0x7f050003;
     }
     public static final class style {
