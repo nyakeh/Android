cac20427d55dbc37cc57654d895d6fc622bd387c
Nyakeh Rogers <nyakeh123@gmail.com>
2014-10-04 21:13:22 +0100

    Added oAuth.io facebook support
    App can now start a fb login intent and return the users account info
    (name, id etc.)

--- Crush/app/src/main/java/uk/co/nyakeh/crush/MainActivity.java
+++ Crush/app/src/main/java/uk/co/nyakeh/crush/MainActivity.java
@@ -1,20 +1,31 @@
 package uk.co.nyakeh.crush;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+import io.oauth.OAuth;
+import io.oauth.OAuthCallback;
+import io.oauth.OAuthData;
+import io.oauth.OAuthRequest;
 
 public class MainActivity extends Activity implements DataLoaderFragment.ProgressListener {
     private static final String TAG_DATA_LOADER = "dataLoader";
@@ -139,9 +150,71 @@ public class MainActivity extends Activity implements DataLoaderFragment.Progres
                     String email = ET_email.getText().toString();
                     String password = ET_password.getText().toString();
 
-                    // TODO Make aSync oAuth request
                     Toast toast = Toast.makeText(v.getContext(), "Nice Try " + email + " : " + password, Toast.LENGTH_LONG);
                     toast.show();
+
+                    final OAuth oauth = new OAuth(getActivity());
+                    oauth.initialize("BpuDpu-omlcZqSRanOmIud3W7Ng");
+                    JSONObject options = new JSONObject();
+                    try {
+                        options.put("username", email);
+                        options.put("password", password);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    OAuthCallback callback = new OAuthCallback() {
+                        @Override
+                        public void onFinished(OAuthData data) {
+                            if (!data.status.equals("success")) {
+                                Toast toast = Toast.makeText(rootView.getContext(), "Fail: " + data.error, Toast.LENGTH_LONG);
+                                toast.show();
+                            }
+                            // Let's skip the NetworkOnMainThreadException for the purpose of this sample.
+                            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitAll().build());
+
+                            data.http(data.provider.equals("facebook") ? "/me" : "/1.1/account/verify_credentials.json", new OAuthRequest() {
+                                private URL url;
+                                private URLConnection con;
+
+                                @Override
+                                public void onSetURL(String _url) {
+                                    try {
+                                        url = new URL(_url);
+                                        con = url.openConnection();
+                                    } catch (Exception e) { e.printStackTrace(); }
+                                }
+
+                                @Override
+                                public void onSetHeader(String header, String value) {
+                                    con.addRequestProperty(header, value);
+                                }
+
+                                @Override
+                                public void onReady() {
+                                    try {
+                                        BufferedReader r = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                                        StringBuilder total = new StringBuilder();
+                                        String line;
+                                        while ((line = r.readLine()) != null) {
+                                            total.append(line);
+                                        }
+                                        JSONObject result = new JSONObject(total.toString());
+
+                                        Toast toast = Toast.makeText(rootView.getContext(), "Hello Inside" +result.getString("name") + "     " + result, Toast.LENGTH_LONG);
+                                        toast.show();
+                                    } catch (Exception e) { e.printStackTrace(); }
+                                }
+
+                                @Override
+                                public void onError(String message) {
+                                    Toast toast = Toast.makeText(rootView.getContext(), "Error " +message, Toast.LENGTH_LONG);
+                                    toast.show();
+                                }
+                            });
+                        }
+                    };
+                    oauth.popup("facebook", options, callback);
+
                 }
             });
 
