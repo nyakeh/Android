e9acebef4fe133bbc044252c7e677ce1a641a545
Nyakeh Rogers <nyakeh123@gmail.com>
2015-07-12 14:40:31 +0100

    you can extend your own classes or language types.

--- ProjectKotlin/app/src/androidTest/java/uk/co/nyakeh/projectkotlin/extensionFunctions.kt
+++ ProjectKotlin/app/src/androidTest/java/uk/co/nyakeh/projectkotlin/extensionFunctions.kt
@@ -1,5 +1,5 @@
 package uk.co.nyakeh.projectkotlin
-
+// You can create you're own extension methods for any class or type!!
 fun stuff(args: Array<String>) {
     var result = "This is my text".convertSpacerToUnderscores()
     println(result)
