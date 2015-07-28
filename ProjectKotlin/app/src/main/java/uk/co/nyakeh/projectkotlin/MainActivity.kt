ca1ff9c25b4ed566984c7956553983aa6cd90217
Nyakeh Rogers <nyakeh123@gmail.com>
2015-07-28 17:54:18 +0100

    Using Anko to access and manipulate view(xml) objects.

--- ProjectKotlin/app/src/main/java/uk/co/nyakeh/projectkotlin/MainActivity.kt
+++ ProjectKotlin/app/src/main/java/uk/co/nyakeh/projectkotlin/MainActivity.kt
@@ -4,12 +4,15 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import kotlinx.android.synthetic.activity_main.*
+import org.jetbrains.anko.text
 
 public class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        message.text = "Hello Kotlin!"
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
