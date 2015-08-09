3bc2bddde6e52f814f77b64e87b2b0f2d2e74f95
Nyakeh Rogers <nyakeh123@gmail.com>
2015-08-09 20:38:24 +0100

    Overrode the default list get method #hype. a[i] --invokes-> a.get(i)

--- ProjectKotlin/app/src/main/java/uk/co/nyakeh/projectkotlin/domain/model/DomainClasses.kt
+++ ProjectKotlin/app/src/main/java/uk/co/nyakeh/projectkotlin/domain/model/DomainClasses.kt
@@ -4,9 +4,8 @@ data class ForecastList(val city: String, val country: String, val dailyForecast
 
     public fun get(position: Int): Forecast = dailyForecast[position]
 
-    public fun size(): Int {
-        return dailyForecast.size()
-    }
+    public fun size(): Int = dailyForecast.size()
+
 }
 
 data class Forecast(val date: String, val description: String, val high: Int, val low: Int)
