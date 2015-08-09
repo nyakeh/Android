92128ef0df9e6ec14984f6dfc9d245a07ce31465
Nyakeh Rogers <nyakeh123@gmail.com>
2015-08-09 20:34:55 +0100

    Implemented functions to access thing that the recyclerView's adapter needs  easier.

--- ProjectKotlin/app/src/main/java/uk/co/nyakeh/projectkotlin/domain/model/DomainClasses.kt
+++ ProjectKotlin/app/src/main/java/uk/co/nyakeh/projectkotlin/domain/model/DomainClasses.kt
@@ -1,5 +1,12 @@
 package uk.co.nyakeh.projectkotlin.domain.model
 
-data class ForecastList(val city: String, val country: String, val dailyForecast: List<Forecast>)
+data class ForecastList(val city: String, val country: String, val dailyForecast: List<Forecast>) {
+
+    public fun get(position: Int): Forecast = dailyForecast[position]
+
+    public fun size(): Int {
+        return dailyForecast.size()
+    }
+}
 
 data class Forecast(val date: String, val description: String, val high: Int, val low: Int)
