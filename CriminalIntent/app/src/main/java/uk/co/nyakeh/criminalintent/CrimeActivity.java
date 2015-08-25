58c56bdfa6cda56dda44d77701aaeabb94a579a4
Nyakeh Rogers <nyakeh123@gmail.com>
2015-08-25 01:12:55 +0100

    Fragment is now more independent from the activity that calls it as now uses the more generic bundle retrieval and its own const String to retrieve the crimeId. The activity however must specifically know how to implement the fragment it wishes to use (which makes sense).

--- CriminalIntent/app/src/main/java/uk/co/nyakeh/criminalintent/CrimeActivity.java
+++ CriminalIntent/app/src/main/java/uk/co/nyakeh/criminalintent/CrimeActivity.java
@@ -7,7 +7,7 @@ import android.support.v4.app.Fragment;
 import java.util.UUID;
 
 public class CrimeActivity extends SingleFragmentActivity {
-    public static final String EXTRA_CRIME_ID = "uk.co.nyakeh.criminalintent.crime_id";
+    private static final String EXTRA_CRIME_ID = "uk.co.nyakeh.criminalintent.crime_id";
 
     public static Intent newIntent(Context packageContext, UUID crimeId) {
         Intent intent = new Intent(packageContext, CrimeActivity.class);
@@ -17,6 +17,7 @@ public class CrimeActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        UUID crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
+        return CrimeFragment.newInstance(crimeId);
     }
 }
--- CriminalIntent/app/src/main/java/uk/co/nyakeh/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/uk/co/nyakeh/criminalintent/CrimeFragment.java
@@ -13,21 +13,31 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.UUID;
 
 public class CrimeFragment extends Fragment {
+    private static final String ARG_CRIME_ID = "crime_id";
+
     private Crime mCrime;
     private EditText mTitleField;
     private Button mDateButton;
     private CheckBox mSolvedCheckbox;
 
+    public static CrimeFragment newInstance(UUID crimeId) {
+        Bundle arguments = new Bundle();
+        arguments.putSerializable(ARG_CRIME_ID, crimeId);
+
+        CrimeFragment fragment = new CrimeFragment();
+        fragment.setArguments(arguments);
+        return fragment;
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        UUID crimeId = (UUID) getActivity().getIntent().getSerializableExtra(CrimeActivity.EXTRA_CRIME_ID);
+        UUID crimeId = (UUID) getArguments().getSerializable(ARG_CRIME_ID);
         mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
     }
 
