342cc31c249034adc73abc94eff98d4c2e61473c
Nyakeh Rogers <nyakeh123@gmail.com>
2015-08-26 18:57:40 +0100

    Display the current crime count when the user asks for it.

--- CriminalIntent/app/src/main/java/uk/co/nyakeh/criminalintent/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/uk/co/nyakeh/criminalintent/CrimeListFragment.java
@@ -3,6 +3,7 @@ package uk.co.nyakeh.criminalintent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -49,6 +50,15 @@ public class CrimeListFragment extends Fragment {
         }
     }
 
+    private void updateSubtitle() {
+        CrimeLab crimeLab = CrimeLab.get(getActivity());
+        int crimeCount = crimeLab.getCrimes().size();
+        String subtitle = getString(R.string.subtitle_format, crimeCount);
+
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        activity.getSupportActionBar().setSubtitle(subtitle);
+    }
+
     @Override
     public void onResume() {
         super.onResume();
@@ -69,6 +79,9 @@ public class CrimeListFragment extends Fragment {
                 CrimeLab.get(getActivity()).addCrime(crime);
                 Intent intent = CrimePagerActivity.newIntent(getActivity(), crime.getId());
                 startActivity(intent);
+                return true;
+            case R.id.menu_item_show_subtitle:
+                updateSubtitle();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
