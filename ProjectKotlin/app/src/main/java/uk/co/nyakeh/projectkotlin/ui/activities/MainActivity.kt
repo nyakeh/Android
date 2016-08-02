180f108ca0e857b127636ea6dafb81ff61408d69
Nyakeh Rogers <nyakeh123@gmail.com>
2016-08-02 21:09:43 +0100

    Chapter 15: Using the kotlin-android-extensions plugin to simplify syntax and remove the need to 'find' view (xml) properties.

--- ProjectKotlin/app/src/main/java/uk/co/nyakeh/projectkotlin/ui/activities/MainActivity.kt
+++ ProjectKotlin/app/src/main/java/uk/co/nyakeh/projectkotlin/ui/activities/MainActivity.kt
@@ -3,9 +3,8 @@ package uk.co.nyakeh.projectkotlin.ui.activities
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.async
-import org.jetbrains.anko.find
 import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
 import uk.co.nyakeh.projectkotlin.R
@@ -16,7 +15,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val forecastList: RecyclerView = find(R.id.forecast_list)
+
         forecastList.layoutManager = LinearLayoutManager(this)
 
         async() {
--- ProjectKotlin/app/src/main/java/uk/co/nyakeh/projectkotlin/ui/adapters/ForecastListAdapter.kt
+++ ProjectKotlin/app/src/main/java/uk/co/nyakeh/projectkotlin/ui/adapters/ForecastListAdapter.kt
@@ -4,10 +4,8 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import com.squareup.picasso.Picasso
-import org.jetbrains.anko.find
+import kotlinx.android.synthetic.main.item_forecast.view.*
 import uk.co.nyakeh.projectkotlin.R
 import uk.co.nyakeh.projectkotlin.domain.model.Forecast
 import uk.co.nyakeh.projectkotlin.domain.model.ForecastList
@@ -27,28 +25,14 @@ class ForecastListAdapter(val weekForecast: ForecastList, val itemClick: (Foreca
 
     override fun getItemCount() = weekForecast.size()
 
-    class ViewHolder(view: View, val itemClick: (Forecast) -> Unit) : RecyclerView.ViewHolder(view){
-        private val iconView: ImageView
-        private val dateView: TextView
-        private val descriptionView: TextView
-        private val maxTemperatureView: TextView
-        private val minTemperatureView: TextView
-
-        init {
-            iconView = view.find(R.id.icon)
-            dateView = view.find(R.id.date)
-            descriptionView = view.find(R.id.description)
-            maxTemperatureView = view.find(R.id.maxTemperature)
-            minTemperatureView = view.find(R.id.minTemperature)
-        }
-
+    class ViewHolder(view: View, val itemClick: (Forecast) -> Unit) : RecyclerView.ViewHolder(view) {
         fun bindForecast(forecast: Forecast) {
             with(forecast) {
-                Picasso.with(itemView.ctx).load(iconUrl).into(iconView)
-                dateView.text = date
-                descriptionView.text = description
-                maxTemperatureView.text = "$high"
-                minTemperatureView.text = "$low"
+                Picasso.with(itemView.ctx).load(iconUrl).into(itemView.icon)
+                itemView.date.text = date
+                itemView.description.text = description
+                itemView.maxTemperature.text = "${high.toString()}º"
+                itemView.minTemperature.text = "${low.toString()}º"
                 itemView.setOnClickListener { itemClick(this) }
             }
         }
